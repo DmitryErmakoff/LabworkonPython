b97c9294b3f1ec3f4a82ee50a54236a8485813b6
DmitryErmakoff <117264406+DmitryErmakoff@users.noreply.github.com>
2022-11-14 20:38:30 +0500

    Update calculator_part2.py

--- Lab1/calculator_part2.py
+++ Lab1/calculator_part2.py
@@ -1,28 +1,27 @@
-import random
-import statistics
+import random # Подключение модуля рандом
+import statistics # Подключение модуля для вычисления медианы 
 
-rand_list=[]
+rand_list=[] # Создание списка
 n=100
-for i in range(n):
-    rand_list.append(random.randint(3,9))
+for i in range(n): # Задаем цикл с диапозоном от 0 до 100
+    rand_list.append(random.randint(3,9)) # Добавляем в конец списка числа, которые располагаются в диапозоне от 3 до 9 (включая эти значения)
 print(rand_list)
 
 total = 0
-for i in range(0,100):
+for i in range(0,100): # Цикл, при помощи которого идет счет суммы чисел в списке
     total += i
-print("Сумма числе этого массив", total)
+print("Сумма чисел списка", total)
 
-def median_f(rand_list):
-    sortedLst = sorted(rand_list) # Cортировка списка
+def median_f(rand_list): # Функция, необходимая для нахождения медианы 
+    sortedLst = sorted(rand_list) # Cортировка списка 
     lstLen = len(rand_list) # Считаем длину строки 
-    index = (lstLen - 1) // 2
-
+    index = (lstLen - 1) // 2 # Находим элемент располагающийся в середине списка, если же кол-во их четное, то найденный суммируется со следующим и делится на два
     if (lstLen % 2):
         return sortedLst[index]
     else:
         return (sortedLst[index] + sortedLst[index + 1])/2.0
 
-print(median_f(rand_list))
-print(statistics.median(rand_list))
+print(median_f(rand_list)) # Вывод медианы, нейденной через созданную функцию 
+print(statistics.median(rand_list)) # Вывод медианы через готовую функцию 
 
 
